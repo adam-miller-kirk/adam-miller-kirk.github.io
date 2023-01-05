d15fdb0aa9601e1f6f48b3d5950bebf496cd7d6c
Adam <adam_kirk_msc@hotmail.com>
2023-01-05 13:13:21 +0000

    updated file and included gh-pages

--- src/utils/help/formatters.js
+++
@@ -1,3 +0,0 @@
-export const formatContactDetails = contact => {
-  return contact;
-}
--- src/utils/weather/formatters.js
+++
@@ -1,4 +0,0 @@
-export const formatWeatherDescription = weatherDescriptions => {
-  // TODO example of a formatter (adding some function)
-  return weatherDescriptions[0];
-}
